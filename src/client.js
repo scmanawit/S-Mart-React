8223836a28be8ad640395c00197edcb63fed8a20
scmanawit <s.manawit@gmail.com>
2023-02-23 09:28:05 +0800

    add onrender api

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const client = () => {
     const token = localStorage.getItem('token')
     const defaultOptions = {
-        baseURL: `${import.meta.env.VITE_API_URL}`,
+        baseURL: https://capstone-3-api.onrender.com,
         headers: {
             'Authorization': token ? `Bearer ${token}` : undefined,
             'Accept': 'application/json',
